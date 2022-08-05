7b38bc0ce9d20a27ef62306036bb67a161a7fbae
Abdelaziz <abdelaziz@pop-os.localdomain>
2022-08-05 19:45:19 +0100

    ending of task_1.rs

--- src/task_1.rs
+++ src/task_1.rs
@@ -1,3 +1,4 @@
+use std::num::ParseFloatError;
 use rand::Rng;
 
 pub fn run() {
@@ -6,34 +7,49 @@ pub fn run() {
 
     // asking to pay for a random product
     let random: f32 = rand::thread_rng().gen_range(1.00..10.00);
-    let amount = format!("{:.2}", random).trim().parse().unwrap();
+    let cost = format!("{:.2}", random).trim().parse().unwrap();
     println!(
         "This product will cost you: {} €. Or you can press q to quit",
-        amount
+        cost
     );
 
     // user input
     loop {
+        // The user enters the cost of money he/she puts into the vending machine
         let mut line = String::new();
         // read input line string and store it into line
         std::io::stdin().read_line(&mut line).unwrap();
+
         // check if user wants to continue
         if line.trim().to_lowercase() == "q" {
             // confirmation message
-            break;
+            println!("are you sure you want to quit ? y/n (pleas type y for yes or n for no.)");
+            // read input line string and store it into conf
+            let mut conf = String::new();
+            std::io::stdin().read_line(&mut conf).unwrap();
+            if conf.trim().to_lowercase() == "y" {
+                break;
+            }
         }
 
-        // convert line to integer
-        let number: f32 = line
+        // check if user input is valid
+        // convert inpute to integer
+        let number: Result<f32, ParseFloatError> = line
             .trim()
-            .parse()
-            .expect("the amount should be in this format 000.00.");
-        
-        
-        if number <= 999.99 && (number * 100.0).fract() == 0.0 && number >= amount {
-            println!("it worked {}", number);
-            let change = number - amount;
-            println!("the change = {:.1$}", change, 2);
+            .parse::<f32>();
+        let amount: f32 = match number {
+            Ok(nb) => nb,
+            Err(_) => {println!("the amount should be a float number in this format 000.00."); continue;},
+        };
+
+        // the amount should be a float number in this format 000.00.
+        if amount <= 999.99 && (amount * 100.0).fract() == 0.0 && amount >= cost {
+            let change = amount - cost;
+            println!("thanks for paying, you can take your product.");
+            if change > 0. {println!("and here's your change : {:.1$}", change, 2);}
+            break;
+        }else {
+            println!("the amount should be in this format 000.00.");
         }
     }
 }
