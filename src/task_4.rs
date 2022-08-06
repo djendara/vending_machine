4c384edee60288013754e8b357cd4ebd00ff3587
Abdelaziz <abdelaziz@pop-os.localdomain>
2022-08-06 17:26:55 +0100

    adding first step for task 4

--- src/task_4.rs
+++ src/task_4.rs
@@ -3,50 +3,34 @@ use std::num::{ParseFloatError, ParseIntError};
 pub fn run() {
     // starting the machine
     println!("~~ Welcom to our vending machine !! ~~");
+    let mut cash_box:Vec<u32> = vec![20; 8];
+    let coins: Vec<f32> = vec![2.00, 1.00, 0.50, 0.20, 0.10, 0.05, 0.02, 0.01];
 
-    // presenting the menu
-    let products = vec![("product 1", 5.99), ("product 2", 6.69), ("product 2", 6.69), ("product 2", 6.69)];
-    for prod in &products{
-        println!("{:?}", prod);
-    }
+    // product selection
+    let cost = select();
+    // paying for the product
+    pay(cost, coins, cash_box);
+}
 
-    // choosing a product
-    println!("choose a product number");
-    let cost: f32; 
-    loop {
-        let mut input = String::new();
-        // read input line string and store it into line
-        std::io::stdin().read_line(&mut input).unwrap();
-        let prod_nb: Result<u32, ParseIntError> = input.trim().parse::<u32>();
-        let nbr: u32 = match prod_nb {
-            Ok(nb) =>nb,
-            Err(_) => 200,
-            
-        };
-        if nbr < 10{
-            cost = products[nbr as usize].1;
-            break;
-        }else {
-            println!("the amount should be an integer number for the product number")
-        }
-
-    }
-    // asking to pay for a random product
-
+pub fn pay(cost: f32, coins: Vec<f32>, cash_box: Vec<u32>) {
+    // asking to pay for the product
     println!(
         "This product will cost you: {} €. Or you can press q to quit",
         cost
     );
 
     // user input
-    loop {
-        // The user enters the cost of money he/she puts into the vending machine
-        let mut line = String::new();
-        // read input line string and store it into line
-        std::io::stdin().read_line(&mut line).unwrap();
+    println!("enter the count number that you have for each coin");
+    let mut amount: f32 = 0.0;
+    let mut update_cash:Vec<u32> = vec![0; 8];
+    for i in 0..8 {
+        println!("how much of this coin do you have {}€", coins[i]);
+        // read the count of this coin
+        let mut count = String::new();
+        std::io::stdin().read_line(&mut count).unwrap();
 
-        // check if user wants to continue
-        if line.trim().to_lowercase() == "q" {
+        // check if user wants to continue or not
+        if count.trim().to_lowercase() == "q" {
             // confirmation message
             println!("are you sure you want to quit ? y/n (pleas type y for yes or n for no.)");
             // read input line string and store it into conf
@@ -57,48 +41,86 @@ pub fn run() {
             }
         }
 
-        // check if user input is valid
-        // convert inpute to integer
-        let number: Result<f32, ParseFloatError> = line.trim().parse::<f32>();
-        let amount: f32 = match number {
-            Ok(nb) => nb,
-            Err(_) => {
-                println!("the amount should be a float number in this format 000.00.");
-                continue;
-            }
-        };
-
-        // the amount should be a float number in this format 000.00.
-        if amount <= 999.99 && (amount * 100.0).fract() == 0.0 && amount >= cost {
-            let change = amount - cost;
-            println!("thanks for paying, you can take your product.");
-            if change > 0. {
-                println!("and here's your change : {:.1$}", change, 2);
-                let list = changecoins(change);
-                println!("{:?}", list);
-                
-            }
-            break;
-        } else {
-            println!("the amount should be in this format 000.00.");
+        // check if user input is valid ?
+        if (count.trim().parse::<u32>().unwrap() + cash_box[i]) <= 50 {
+            update_cash[i] = update_cash[i] + count.trim().parse::<u32>().unwrap();
+            amount = amount + coins[i] * count.trim().parse::<f32>().unwrap();
+        }else {
+            // true false
         }
+    }
+
+    // the amount should be a float number in this format 000.00.
+    if amount <= 999.99 && amount >= cost {
+        let change = amount - cost;
+        // println!("thanks for paying, you can take your product.");
+        if change > 0. {
+            println!("and here's your change : {:.1$}", change, 2);
+            let list = changecoins(change, coins, cash_box);
+            println!("{:?}", list);
+        }
+        // break;
+    } else {
+        println!("the amount should be in this format 000.00.");
     }
 }
 
-pub fn changecoins(number: f32) -> Vec<(f32, i32)> {
-    let coins: Vec<f32> = vec![2.00, 1.00, 0.50, 0.20, 0.10, 0.05, 0.02, 0.01];
-    let mut list = vec![];
-    let mut rest: f32 = number;
-    for x in coins {
-        let div: f32 = format!("{:.2}", rest / x).trim().parse().unwrap();
-        let int_part = div as i32;
-        if div.fract() == 0.0 {
-            list.push((x, int_part));
+pub fn select() -> f32 {
+    // presenting the menu
+    let products = vec![
+        ("product 1", 5.99),
+        ("product 2", 6.69),
+        ("product 2", 6.69),
+        ("product 2", 6.69),
+    ];
+    for prod in &products {
+        println!("{:?}", prod);
+    }
+
+    // choosing a product
+    println!("choose a product number");
+    let cost: f32;
+    loop {
+        let mut input = String::new();
+        // read input line string and store it into line
+        std::io::stdin().read_line(&mut input).unwrap();
+        let prod_nb: Result<u32, ParseIntError> = input.trim().parse::<u32>();
+        let nbr: u32 = match prod_nb {
+            Ok(nb) => nb,
+            Err(_) => 200,
+        };
+        if nbr < 10 {
+            cost = products[(nbr as usize)-1].1;
             break;
-        } else if int_part != 0 {
-            list.push((x, int_part));
-            rest = rest - x * (int_part as f32);
+        } else {
+            println!("the amount should be an integer number for the product number")
         }
     }
-    return list;
+    return cost;
+}
+
+pub fn changecoins(number: f32, coins: Vec<f32>, cash_box: Vec<u32>) -> Vec<(f32, u32)> {
+    let mut list = vec![];
+    let mut rest: f32 = number;
+    for i in 0..8 {
+        let div: f32 = format!("{:.2}", rest / coins[i]).trim().parse().unwrap();
+        let mut int_part = div as u32;
+        if cash_box[i] == 0 || int_part == 0{
+            continue;
+        }
+        if int_part > cash_box[i]{
+            int_part = cash_box[i];
+        }
+        
+        list.push((coins[i], int_part));
+        rest = rest - coins[i] * (int_part as f32);
+       
+        if rest == 0.0 {
+            break;
+        } 
+    }
+
+    // if rest != 0.0 {list=[].to_vec();}
+    
+    return  list;
 }
